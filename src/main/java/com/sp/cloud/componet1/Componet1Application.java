9bd89ea2125643e2bc7329e8532d4859d68ca4f9
Nikhil Y <Nikhil.Y@mphasis.com>
2017-09-17 23:29:19 +0530

    initial files

---
+++ src/main/java/com/sp/cloud/componet1/Componet1Application.java
@@ -0,0 +1,12 @@
+package com.sp.cloud.componet1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Componet1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Componet1Application.class, args);
+	}
+}
