46c0dbe4e23196471ef51497bb8a43337d1a5f17
d568056 <31965410+d568056@users.noreply.github.com>
2017-12-25 12:58:45 +0530

    Update Componet1Application.java

--- src/main/java/com/sp/cloud/componet1/Componet1Application.java
+++ src/main/java/com/sp/cloud/componet1/Componet1Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Componet1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Componet1Application.class,args);
+		SpringApplication.run(Componet1Application.class, args);
 	}
 }
