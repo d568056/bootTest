bb8f2e05cc0575d38a7d7d0c2b539178ab992c3e
nikhilyadav <menikhil59@gmail.com>
2020-01-11 21:07:25 -0500

    added size check in mockmvc test

--- src/test/java/com/sp/cloud/controller/CarControllerTest.java
+++ src/test/java/com/sp/cloud/controller/CarControllerTest.java
@@ -1,20 +1,25 @@
 package com.sp.cloud.controller;
 
 import com.sp.cloud.conf.ApplicationConfiguration;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import  org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.util.regex.Matcher;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(CarController.class)
 @ContextConfiguration(classes={ApplicationConfiguration.class})
 public class CarControllerTest {
@@ -25,9 +30,11 @@ public class CarControllerTest {
     @Test
     public void getCar_returnCarDetails() throws Exception{
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/car/prius")).andExpect(status().isOk())
+        mockMvc.perform(get("/car/prius"))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("name").value("prius"))
-                .andExpect(jsonPath("type").value("hybrid"));
+                .andExpect(jsonPath("$.type").value("hybrid"))
+                .andExpect(jsonPath("$.*",Matchers.hasSize(2)));
 
     }
 }
